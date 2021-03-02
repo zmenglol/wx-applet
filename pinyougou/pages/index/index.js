eb78471c33ef688fc28b9d8a303df8f97ebb25be
zmenglol <1394443256@qq.com>
2021-03-02 23:21:34 +0800

    20210302

--- pinyougou/pages/index/index.js
+++ pinyougou/pages/index/index.js
@@ -1,39 +1,51 @@
 //Page Object
+import { request } from "../../request/index.js"
 Page({
   data: {
-    
+    //轮播图数组
+    swiperList:[],
+    //导航数组
+    catesList:[],
+    //楼层数组
+    floorList:[]
   },
-  //options(Object)
-  onLoad: function(options) {
-    
-  },
-  onReady: function() {
-    
-  },
-  onShow: function() {
-    
-  },
-  onHide: function() {
+  onLoad: function(options){
+    // 1 发送异步请求获取轮播图数据 优化的手段可以通过es6的 Promise
 
+    // var reqTask = wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     console.log(result);
+    //     this.setData({
+    //       swiperList:result.data.message 
+    //   }
+    // });
+    this.getSwiperList();
+    this.getCatesList();
+    this.getFloorList();
   },
-  onUnload: function() {
-
+  getSwiperList(){
+    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
+      .then(result => {
+        this.setData({
+          swiperList:result.data.message
+        })
+      })
   },
-  onPullDownRefresh: function() {
-
+  getCatesList(){
+    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'})
+      .then(result => {
+        this.setData({
+          catesList:result.data.message
+        })
+      })
   },
-  onReachBottom: function() {
-
-  },
-  onShareAppMessage: function() {
-
-  },
-  onPageScroll: function() {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item) {
-
+  getFloorList(){
+    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'})
+      .then(result => {
+        this.setData({
+          floorList:result.data.message
+        })
+      })
   }
 });
-  
