229a042afeda6e8b4099fd2a6f62dba00948dede
lukelong <1258828871@qq.com>
2021-05-02 21:59:37 +0800

    test pull request

--- pinyougou/app.js
+++ pinyougou/app.js
@@ -2,7 +2,7 @@
 App({
     //onLaunch,onShow: options(path,query,scene,shareTicket,referrerInfo(appId,extraData))
     onLaunch: function(options) {
-        
+        //test pull request
     },
     onShow: function(options) { 
 
